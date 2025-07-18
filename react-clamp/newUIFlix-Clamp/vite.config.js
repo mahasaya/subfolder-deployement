492861e0f38786cb30a8d2ede08e31f063b3dc3e
Bhawya Misri <bmisri@tran.ad.com>
2025-07-18 17:50:12 +0530

    Build for GitHub Pages

--- react-clamp/newUIFlix-Clamp/vite.config.js
+++ react-clamp/newUIFlix-Clamp/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: '/subfolder-deployement/react-clamp/newUIFlix-Clamp', 
+  base: '/react-clamp/newUIFlix-Clamp/', 
   build: {
     outDir: 'docs',
   },
