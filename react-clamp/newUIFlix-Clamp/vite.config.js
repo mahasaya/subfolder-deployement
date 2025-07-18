d9767f980161abb901b694d89a160cb4d1dda331
Bhawya Misri <bmisri@tran.ad.com>
2025-07-18 16:26:35 +0530

    deploy

--- react-clamp/newUIFlix-Clamp/vite.config.js
+++ react-clamp/newUIFlix-Clamp/vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
   base: '/subfolder-deployement/react-clamp/newUIFlix-Clamp', 
+  build: {
+    outDir: 'docs',
+  },
 })
